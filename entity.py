c689493ef2d35bbe0c728fe0a12369584a837644
Nick Perkins <nick@nicholasperkins.com>
2023-03-11 12:13:33 +1000

    Part 2 - The generic Entity, the render functions, and the map

---
+++ entity.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+
+class Entity:
+    """
+    A generic object to represent players, enemies, items, etc.
+    """
+
+    def __init__(self, x: int, y: int, char: str, colour: Tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.colour = colour
+
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
