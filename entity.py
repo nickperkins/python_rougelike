ff3ce227ddc95898ba3b1482e8b4d1a09992dfb7
Nick Perkins <nick@nicholasperkins.com>
2023-03-25 08:02:21 +1000

    Part 9 - Ranged Scrolls and Targeting

--- entity.py
+++ entity.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import copy
+import math
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 
 from render_order import RenderOrder
@@ -68,6 +69,12 @@ class Entity:
                     self.game_map.entities.remove(self)
             self.parent = game_map
             game_map.entities.add(self)
+
+    def distance(self, x: int, y: int) -> float:
+        """
+        Return the distance between the current entity and the given (x, y) coordinates
+        """
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
 
     def move(self, dx: int, dy: int) -> None:
         """Move the entity by a given amount"""
